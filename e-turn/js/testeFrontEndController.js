492c63f880db3e8573a6cab8733df8cc97d8fdf1
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2020-09-21 14:43:08 -0300

    e-turn

---
+++ e-turn/js/testeFrontEndController.js
@@ -0,0 +1,496 @@
+dados = [{
+    "id": 1,
+    "nome": "Casaco alfaiataria com bolsos laterais",
+    "categoria": "Casaco",
+    "notas": [
+        {
+            nota: "5"
+        },
+        {
+            nota: "4"
+        },
+        {
+            nota: "2"
+        },
+        {
+            nota: "1"
+        },
+        {
+            nota: "3"
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L1-1.jpg",
+            destacada: false,
+        },
+        {
+            nome: "L1-2.jpg",
+            destacada: true,
+        }
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+}, {
+    "id": 2,
+    "nome": "Blusa com pregas e gola arredondada",
+    "categoria": "Blusa",
+    "notas": [
+        {
+            nota: 4,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 1,
+        },
+        {
+            nota: 5,
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L2-1.jpg",
+            destacada: false
+        },
+        {
+            nome: "L2-2.jpg",
+            destacada: true,
+        },
+        {
+            nome: "L2-3.jpg",
+            destacada: false
+        }
+    ],
+    "precoMedio": 2,
+    "disponivel": true
+}, {
+    "id": 3,
+    "nome": "Vestido midi tubinho",
+    "categoria": "Vestido",
+    "notas": [
+        {
+            nota: 4,
+        },
+    ],
+    "imagens": [
+        {
+            nome: "L3-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 4,
+    "disponivel": false
+}, {
+    "id": 4,
+    "nome": "Calça social alfaiataria com bolsos",
+    "categoria": "Calça",
+    "notas": [
+        {
+            nota: 5,
+        },
+        {
+            nota: 3
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L4-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": true
+}, {
+    "id": 5,
+    "nome": "Blusa de algodão com estampa de sorvete",
+    "categoria": "Blusa",
+    "notas": [
+        {
+            nota: 4,
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L5-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 1,
+    "disponivel": false
+}, {
+    "id": 6,
+    "nome": "Body preto básico",
+    "categoria": "Body",
+    "notas": [
+        {
+            nota: 1,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 2,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 5
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L6-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+}, {
+    "id": 7,
+    "nome": "Body cavado com corte",
+    "categoria": "Body",
+    "notas": [
+        {
+            nota: 1,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 2,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 5
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L7-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+}, {
+    "id": 8,
+    "nome": "Blusa ciganinha branca com elástico",
+    "categoria": "Blusa",
+    "notas": [
+        {
+            nota: 4,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 4
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L8-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 2,
+    "disponivel": false
+}, {
+    "id": 9,
+    "nome": "Casaco alfaiataria slim com bolsos",
+    "categoria": "Casaco",
+    "notas": [
+        {
+            nota: 5,
+        },
+        {
+            nota: 5
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L9-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 4,
+    "disponivel": true
+},
+{
+    "id": 10,
+    "nome": "Vestido branco com detalhes em renda",
+    "categoria": "Vestido",
+    "notas": [
+        {
+            nota: 1,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 2,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 5
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L10-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 2,
+    "disponivel": false
+}, {
+    "id": 11,
+    "nome": "Blusa branca com corte",
+    "categoria": "Blusa",
+    "notas": [
+        {
+            nota: 5,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 4,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 5
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L11-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 1,
+    "disponivel": true
+}, {
+    "id": 12,
+    "nome": "Calça jeans com rasgos e detalhes",
+    "categoria": "Calça",
+    "notas": [
+    ],
+    "imagens": [
+        {
+            nome: "L12-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+}, {
+    "id": 9,
+    "nome": null,
+    "categoria": null,
+    "notas": [
+        {
+            nota: 3,
+        },
+        {
+            nota: 3,
+        },
+        {
+            nota: 5,
+        },
+        {
+            nota: 2,
+        },
+        {
+            nota: 3
+        },
+        {
+            nota: 3
+        }
+    ],
+    "imagens": [
+        {
+            nome: "L12-1.jpg",
+            destacada: true,
+        },
+    ],
+    "precoMedio": 3,
+    "disponivel": false
+}]
+
+
+angular.module("testeFront").controller("testeFrontEndController", function ($scope, $http, $document) {
+
+    // changeDisp();
+
+    console.log(angular.element(document.querySelector('#disponible')).find('.disponibilidade').css({ 'opacity': 0.4 }));
+
+    var categoriasProdutos = [];
+    var corDisponibilidade;
+    dados.forEach(element => {
+        element.imagens.forEach(e => {
+            e.nome = "produtos/" + e.nome;
+        })
+
+        categoriasProdutos.push(element.categoria);
+
+        if(element.disponivel){
+            corDisponibilidade = "green";
+        }
+        else{
+            corDisponibilidade = "red";
+        }
+
+        console.log(corDisponibilidade)
+
+        let pMedio = element.precoMedio;
+
+        switch (pMedio) {
+            case 1:
+                element.precoMedio = "$";
+                break;
+            case 2:
+                element.precoMedio = "$$";
+                break;
+            case 3:
+                element.precoMedio = "$$$";
+                break;
+            case 4:
+                element.precoMedio = "$$$$";
+                break;
+            case 5:
+                element.precoMedio = "$$$$$";
+                break;
+            default:
+                element.precoMedio = "-";
+                break;
+        }
+
+        // funcao para percorrer o vetor de notas para montagem da url para get de api
+        let avaliacao = "";
+        let avaliacaoFinal = "";
+        let qtdAval = 0;
+        element.notas.forEach(e => {
+            avaliacao += e.nota + ",";
+            qtdAval++;
+        });
+
+
+        // avalicaoFinal recebe a montagem da url para API
+        avaliacaoFinal = "http://api.mathjs.org/v4/?expr=sum" + "(" + avaliacao.slice(0, -1) + ")" + "/" + qtdAval;
+        $http({
+            method: 'GET',
+            url: avaliacaoFinal
+        }).then(function successCallback(response) {
+
+            element.notas = response.data;
+        }, function errorCallback(response) {
+        });
+
+
+
+    });
+
+
+    $scope.prod = (dados);
+
+    //verificação se a imagem é ou não destacada
+    $scope.imgDestacada = function (item) {
+        if (item.destacada) {
+            return true;
+        }
+    }
+
+    // $scope.isDisponible = function (item) {
+    //     if (item.disponivel) {
+    //         return true;
+    //     }
+    // }
+
+    $scope.corDisponibilidade = function (item) {
+        if (item.disponivel) {
+            $scope.customStyle.colorClass = "green";
+        }
+        else {
+            $scope.customStyle.colorClass = "red";
+        }
+    }
+
+    $scope.precos = [
+        { nome: "$", codigo: 1 },
+        { nome: "$$", codigo: 2 },
+        { nome: "$$$", codigo: 3 },
+        { nome: "$$$$", codigo: 4 },
+        { nome: "$$$$$", codigo: 5 },
+    ]
+
+    var categoriasFinal = categoriasProdutos.filter(function (elem, index, self) {
+        return index === self.indexOf(elem || !null);
+    });
+
+
+    $scope.categorias = categoriasFinal;
+
+
+
+    function changeDisp() {
+        data.forEach(e => {
+            console.log("a")
+            if (e.innerHTML == "true") {
+                console.log("entrou")
+                e.innerHTML = "Disponível"
+            }
+            else {
+                e.innerHTML = "Indisponível"
+            }
+        });
+    }
+});
---
+++ e-turn/readme.txt
@@ -0,0 +1,12 @@
+Para visualiar o projeto somente é necessário abrir o arquivo index.html.
+
+Foi utilizada a biblioteca css Boostratp, todo restante foi feito utilizando javascript e angularJs.
+
+
+
+Obervações:
+
+1 - Ao ler o arquivo produtos.js obtive um erro de CorsOrigin, não consegui resolve-lo, já tive alguns problema por causa do meu pc, com isto, 
+para conseguir fazer o restante do teste coloquei o arquivo diretamente no arquivo testeFrontEndController.js
+
+
