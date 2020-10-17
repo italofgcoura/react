b807df9eb2536051d77d4a7b30363c62648f870b
Arthur Vinícius <arthurvcs2@gmail.com>
2020-10-16 22:30:02 -0300

    tá ái meu parça

--- ReactJS/src/components/clientes/Clientes.jsx
+++ ReactJS/src/components/clientes/Clientes.jsx
@@ -1,87 +1,45 @@
-// src/components/contacts.js
-
 import React, { useEffect, useState } from "react";
 import TituloTeste from '../titulos/titulos'
 
+const Clientes = () => {
+    
+    const [usuario, setUsers] = useState([])
 
-export default function Clientes() {
-  const [clientes, setCliente] = useState([{
-    CPF: 123456789,
-    Nome: "Italo",
-    ID: 1
-  }, {
-    CPF: 1234567789,
-    Nome: "AItalo",
-    ID: 2
-  }, {
-    CPF: 1263456789,
-    Nome: "IBtalo",
-    ID: 3
-  }]);
+    useEffect(() => {
+        getData(setUsers);
+    }, []);
 
-  const [usuario, setUsers] = useState([])
+    const getData =(setUsers) => {
+        fetch("http://localhost:3000/users")
+        .then((res) => res.json())
 
-  
+        .then((result) => {
+        setUsers(result);
+        })
+    }
 
-  console.log(usuario)
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>Admin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usuario.map((client, index) => (
+                    <tr key={client.ID}>
+                        <TituloTeste nome={client.Nome}></TituloTeste>
+                        <td>{client.CPF}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
 
-  useEffect(() => {
-    console.log("use efect rodou");
-    getData(setUsers);
-  }, [clientes, usuario]);
-
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>CPF</th>
-            <th>Admin</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clientes.map((client, index) => (
-            <tr key={client.ID}>
-              <TituloTeste nome={client.Nome}></TituloTeste>
-              <td>{client.CPF}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    );
 }
 
-function deleteData(ID) {
-  console.log("delete");
-  const userData = {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  };
-
-  fetch("http://localhost:3000/clientes/" + ID, userData)
-    .then((Response) => console.table("dados" + Response.json()))
-    .catch((error) => console.error("Erro inesperado" + error));
-}
-
-function getData(setUsers) {
-  fetch("http://localhost:3000/users")
-
-    .then((res) => res.json())
-
-    .then((result) => {
-
-      console.log(result)
-
-      setUsers(result);
-
-    })
-
-  // .catch(function (err) {
-  //   console.error("Erro: Verificar rota e/ou api", err);
-  // });
-}
+export default Clientes;
